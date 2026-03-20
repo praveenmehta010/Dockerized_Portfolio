1e78c2e0c6208cd04fd16b557cb1fb810f72d899
Praveen Mehta <praveen.mehta010.com>
2026-03-20 17:49:49 +0530

    MongoDB Atlas connected

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from config.mongo_db_connection import projects
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get('/project/{title}')
+def get_project_by_title(title : str):
+    # Manual testing
+    # Query for a Project that has the title 'Back to the Future'
+    query = { "title": title }
+    project = projects.find_one(query)
+    print(project)
+    
